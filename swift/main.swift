be859798e25856442124100d2bac9cc803078385
601 <601@useallfive.com>
2014-07-30 17:05:57 -0700

    Updated for Beta 3/4 and tweaks

--- swift/main.swift
+++ swift/main.swift
@@ -1,59 +1,60 @@
 
-class Invalidation<T: AnyObject> { // possible Swift bug requires ": AnyObject"
-  
-  let that: T, errors: String[]
-  
-  init(_ t: T, _ e: String[] = []) { that = t; errors = e }
-  
-  subscript(invalid: T -> String?) -> Invalidation {
-    
-    if let error = invalid(that) { return Invalidation(that, [error] + errors) }
-      
+class Invalidation <T> {
+
+  let that: T, errors: [String]
+
+  init (_ t: T, _ e: [String] = []) { (that, errors) = (t, e) }
+
+  subscript (ƒinvalid: T -> String?) -> Invalidation {
+
+    if let error = ƒinvalid(that) { return Invalidation(that, [error] + errors) }
+
       return self
   }
+}
+
+extension Invalidation: Printable {
 
   var description: String { return "Invalidation(\(that), \(errors))" }
 }
 
-extension Invalidation: Printable { }
-
 operator infix <*> { associativity left } // for Hal
 
-func <*> <T: AnyObject> (lhs: Invalidation<T>, rhs: T -> String?) -> Invalidation<T> { return lhs[rhs] }
+func <*> <T> (lhs: Invalidation<T>, rhs: T -> String?) -> Invalidation<T> { return lhs[rhs] }
 
 
-class Person {
-  
+class Person: Printable {
+
   let nom: String, zip: Int, age: Int
-  
-  init(_ n: String, _ z: Int, _ a: Int) { nom = n; zip = z; age = a }
-  
+
+  init (_ n: String, _ z: Int, _ a: Int) { (nom, zip, age) = (n, z, a) }
+
   var description: String { return "Person(\(nom), \(zip), \(age))" }
 }
 
-extension Person: Printable { }
-
-
 let people = [
-  
+
   Person("Brian", 90265, 54), Person("brian", 123456, 105),
 
   Person("Roe", 90100, 5), Person("Joe", 0, 22), Person("", -1, -20), Person("Joe", 99999, 50),
 ]
 
-func isFirstUppercase(s: String) -> Bool { // there MUST be a better way!
-  
-  for ch in s { for up in s.uppercaseString { return ch == up } }; return false
+
+import Foundation
+
+func isFirstUppercase ($: String) -> Bool { // there MUST be a better way!
+
+  for ch in $ { for up in $.uppercaseString { return ch == up } }; return false
 }
 
 for person in people { println(
-  
+
   Invalidation(person)
-    
+
     <*> { let age = $0.age; return !(0 < age && age <= 100) ? "!age" : nil }
-    
+
     <*> { let zip = $0.zip; return zip <= 0 || zip > 99999 ? "!zip" : nil }
-    
+
     <*> { let nom = $0.nom; return !isFirstUppercase(nom) ? "!cap" : nil }
   )
 }
